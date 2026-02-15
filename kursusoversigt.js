678091429f6bf8d47460454e0cb96a5756e6e523
amtssp <amtssp@gmail.com>
2026-02-15 10:47:16 +0100

    Create kursusoversigt.js

---
+++ kursusoversigt.js
@@ -0,0 +1,20 @@
+const kurser = [
+    {
+        navn: "Avanceret Excel Analyse",
+        kortNavn: "Excel Analyse",
+        beskrivelse: "Lær pivottabeller, Power Query og avancerede formler.",
+        link: "https://example.com/excel-analyse"
+    },
+    {
+        navn: "Grundlæggende HTML & CSS",
+        kortNavn: "HTML CSS",
+        beskrivelse: "Introduktion til webudvikling med HTML og CSS.",
+        link: "https://example.com/html-css"
+    },
+    {
+        navn: "JavaScript for Begyndere",
+        kortNavn: "JavaScript",
+        beskrivelse: "Forstå grundlæggende JavaScript og DOM manipulation.",
+        link: "https://example.com/javascript"
+    }
+];
