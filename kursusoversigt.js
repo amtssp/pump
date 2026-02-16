259c644ffdc00cf43a26938528a819629b99a222
amtssp <amtssp@gmail.com>
2026-02-16 07:26:47 +0100

    Update kursusoversigt.js

--- kursusoversigt.js
+++ kursusoversigt.js
@@ -1,9 +1,9 @@
 const kurser = [
     {
-        navn: "Avanceret Excel Analyse",
-        kortNavn: "Excel Analyse",
-        beskrivelse: "Lær pivottabeller, Power Query og avancerede formler.",
-        link: "https://example.com/excel-analyse"
+        navn: "Graviditet og diabetes",
+        kortNavn: "Graviditet og diabetes",
+        beskrivelse: "Se hvordan du kan håndtere din diabetes under en graviditet. Det kan være, at du har diabetes og er gravid eller overvejer at blive det. Læs mere her.",
+        link: "https://diabetes.rm.dk/dine-muligheder/graviditet-og-diabetes/"
     },
     {
         navn: "Grundlæggende HTML & CSS",
