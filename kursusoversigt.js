14ba5dce9fcaf0b6cffc24538f5f5d101cec1b56
amtssp <amtssp@gmail.com>
2026-02-16 07:52:03 +0100

    Update kursusoversigt.js

--- kursusoversigt.js
+++ kursusoversigt.js
@@ -16,37 +16,37 @@ const kurser = [
         kortNavn: "Mental trivsel",
         beskrivelse: "Diabetes kan påvirke dig psykisk og dine tanker og følelser kan også påvirke din diabetes. Hvis du er udfordret af din diabetes, så læs mere her.",
         link: "https://diabetes.rm.dk/dine-muligheder/mental-trivsel/"
-    }
+    },
         {
         navn: "Motion og Type 1 diabetes",
         kortNavn: "Motion og diabetes",
         beskrivelse: "Har du type 1-diabetes og vil du vide mere om, hvordan du kan balancere motion og diabetes? Læs mere her.",
         link: "https://diabetes.rm.dk/dine-muligheder/motion-og-diabetes/"
-    }
+    },
     {
         navn: "Ny med Type 1 diabetes",
         kortNavn: "Ny med diabetes",
         beskrivelse: "Har du fået konstateret type 1-diabetes inden for de sidste 2-12 måneder? Læs om gruppeforløb for nye med type 1-diabetes.",
         link: "https://diabetes.rm.dk/behandling/type-1-diabetes/ny-med-type-1-diabetes/"
-    }
+    },
     {
         navn: "Oversigt over forskellige projekter v Type 2 diabetes",
         kortNavn: "Sammen om T2diabetes",
         beskrivelse: "Læs om sundhedstjeneste- og forebyggelsesforskning hos Steno Diabetes Center Aarhus.",
         link: "https://www.stenoaarhus.dk/tvarsektoriel-forskning/sygdom-og-trivsel/sammen-om-diabetes/"
-    }
+    },
     {
         navn: "Køkken Workshop med Type 1 diabetes",
         kortNavn: "Køkken Workshop",
         beskrivelse: "Har du type 1-diabetes, og vil du gerne vide mere om kulhydrater, mad og blodsukker? Så kom med til køkkenworkshop.",
         link: "https://www.auh.dk/afdelinger/steno-diabetes-center-aarhus-klinik/voksne-med-diabetes/workshop-klogere-pa-kulhydrater/"
-    }
+    },
     {
         navn: "Tilbud om diætistsamtaler",
         kortNavn: "Diætistsamtaler",
         beskrivelse: "Maden spiller en vigtig rolle i behandling af diabetes. Læs her, hvad en diætist kan hjælpe med, når blodsukker, måltider og insulin skal i balance.",
         link: "https://www.auh.dk/afdelinger/steno-diabetes-center-aarhus-klinik/voksne-med-diabetes/diaetist-samtaler/"
-    }
+    },
     {
         navn: "Info om årssamtalen",
         kortNavn: "Info om årssamtalen",
